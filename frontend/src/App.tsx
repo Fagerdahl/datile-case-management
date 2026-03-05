9f3af2da74530c8625fee147e680122a258576e1
ViktorNoskire <vikkerinho@gmail.com>
2026-03-05 11:31:29 +0100

    refactor: Placed all the routes inside a router which makes it easier to maintain

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Errands, Reports, Customers, Users, Settings, PingPage } from "./pages";
-
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./components/AppRoutes";
 export default function App() {
 
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<div>HELLO</div>} />
-                <Route path="/ping" element={<PingPage/>} />
-                <Route path="/errands" element={<Errands/>} />
-                <Route path="/reports" element={<Reports/>} />
-                <Route path="/customers" element={<Customers/>} />
-                <Route path="/users" element={<Users/>} />
-                <Route path="/settings" element={<Settings/>} />
-            </Routes>
+            <AppRoutes/>
         </BrowserRouter>
     );
 }
